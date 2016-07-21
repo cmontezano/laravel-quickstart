6ee8aee27818619faf7c2e0b631b9e1caf113ed5
Carlos Montezano <cmontezanol@gmail.com>
2016-07-21 19:30:06 -0300

    define user relatioship on task

--- app/Task.php
+++ app/Task.php
@@ -7,4 +7,12 @@ use Illuminate\Database\Eloquent\Model;
 class Task extends Model
 {
     protected $fillable = ['name'];
+
+    /**
+     * Get the user that owns the task
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
