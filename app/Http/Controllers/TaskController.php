fb016a445105538bc20866e924d636e0f26e98e3
Carlos Montezano <cmontezanol@gmail.com>
2016-07-21 19:46:27 -0300

    add task controller

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,14 @@
+<?php
+namespace App\Http\Controllers;
+
+use App\Http\Requests;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class TaskController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+}
