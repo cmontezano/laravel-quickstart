5de93a11b830e91bccaa74618d7d0881d33f260e
Carlos Montezano <cmontezanol@gmail.com>
2016-07-21 23:37:28 -0300

    add index action to task controller

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -5,21 +5,21 @@
     <div>
         @include('common.errors')
 
-        <form>
+        <form action="{{ url('task') }}" method="POST" class="form-horizontal">
             {{ csrf_field() }}
 
-            <div>
-                <label>Task</label>
+            <div class="form-group">
+                <label for="task-name">Task</label>
 
-                <div>
-                    <input type="text">
+                <div class="col-sm-6">
+                    <input type="text" name="name" id="task-name" class="form-control">
                 </div>
             </div>
 
-            <div>
-                <div>
-                    <button>
-                        <i></i>
+            <div class="form-group">
+                <div class="col-sm-offset-3 col-sm-6">
+                    <button type="submit" class="btn btn-default">
+                        <i class="fa fa-plus"></i> Add Task
                     </button>
                 </div>
             </div>
