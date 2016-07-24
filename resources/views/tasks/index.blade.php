d5099c7e7532faddeec385d32f19cf7fa09b2a29
Carlos Montezano <cmontezanol@gmail.com>
2016-07-24 00:02:33 -0300

    add html comments and classes

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -2,20 +2,26 @@
 
 @section('content')
 
-    <div>
+    <!-- Bootstrap Boilerplate... -->
+
+    <div class="panel-body">
+        <!-- Display validation errors -->
         @include('common.errors')
 
+        <!-- New task Form -->
         <form action="{{ url('task') }}" method="POST" class="form-horizontal">
             {{ csrf_field() }}
 
+            <!-- Task name -->
             <div class="form-group">
-                <label for="task-name">Task</label>
+                <label for="task-name" class="col-sm-3 control-label">Task</label>
 
                 <div class="col-sm-6">
                     <input type="text" name="name" id="task-name" class="form-control">
                 </div>
             </div>
 
+            <!-- Add task Button -->
             <div class="form-group">
                 <div class="col-sm-offset-3 col-sm-6">
                     <button type="submit" class="btn btn-default">
