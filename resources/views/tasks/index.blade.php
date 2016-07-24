1ab36131e5ac9170b353f2b860e4e28e4b63f683
Carlos Montezano <cmontezanol@gmail.com>
2016-07-24 01:08:18 -0300

    create delete button

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -27,7 +27,14 @@
                                         <div>{{ $task->name }}</div>
                                     </td>
                                     <td>
-                                        <!-- TODO: Delete Button -->
+                                        <form action="{{ url('task/'.$task->id) }}" method="POST">
+                                            {{ csrf_field() }}
+                                            {{ method_field('DELETE') }}
+
+                                            <button type="submit" id="delete-task-{{ $task->id }}" class="btn btn-danger">
+                                                <i class="fa fa-btn fa-trash"></i> Delete
+                                            </button>
+                                        </form>
                                     </td>
                                 </tr>
                             @endforeach
